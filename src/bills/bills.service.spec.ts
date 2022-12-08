b956c7f366fe6f9fad99b5102a4f6c21cd1da652
Julia Ziębińska <julkazbs2000@gmail.com>
2022-12-08 19:35:56 +0100

    test: add tests for bills

--- src/bills/bills.service.spec.ts
+++ src/bills/bills.service.spec.ts
@@ -1,18 +1,70 @@
 import { Test, TestingModule } from '@nestjs/testing'
+import { PartyGroupService } from '../party-group/party-group.service'
+import { BillsDao } from './bills.dao'
 import { BillsService } from './bills.service'
+import { BillCreateInput } from './models/bills.input'
 
 describe('BillsService', () => {
 	let service: BillsService
 
 	beforeEach(async () => {
 		const module: TestingModule = await Test.createTestingModule({
-			providers: [BillsService],
+			providers: [
+				BillsService,
+				{ provide: PartyGroupService, useValue: groupServiceMock },
+				{ provide: BillsDao, useValue: daoMock },
+			],
 		}).compile()
 
 		service = module.get<BillsService>(BillsService)
 	})
 
-	it('should be defined', () => {
-		expect(service).toBeDefined()
+	afterEach(jest.clearAllMocks)
+	afterAll(jest.resetAllMocks)
+
+	describe('billCreate', () => {
+		const input = {
+			groupCode: 'code',
+		} as BillCreateInput
+		it('should call service and dao', async () => {
+			groupServiceMock.findGroupByCode.mockImplementationOnce(() => ({
+				uuid: 'groupUuid',
+			}))
+
+			await service.billCreate(input)
+
+			expect(groupServiceMock.findGroupByCode).toBeCalledTimes(1)
+			expect(groupServiceMock.findGroupByCode).toBeCalledWith('code')
+			expect(daoMock.create).toBeCalledTimes(1)
+			expect(daoMock.create).toBeCalledWith({
+				name: input.name,
+				price: input.price,
+				Payer: { connect: { uuid: input.payedBy } },
+				Group: { connect: { uuid: 'groupUuid' } },
+			})
+		})
 	})
+
+	describe('billGetForGroupUser', () => {
+		it('should call service and dao', async () => {
+			groupServiceMock.findGroupByCode.mockImplementationOnce(() => ({
+				uuid: 'groupUuid',
+			}))
+
+			await service.billGetForGroupUser('code', 'userUuid')
+
+			expect(groupServiceMock.findGroupByCode).toBeCalledTimes(1)
+			expect(groupServiceMock.findGroupByCode).toBeCalledWith('code')
+			expect(daoMock.findByGroupUser).toBeCalledTimes(1)
+			expect(daoMock.findByGroupUser).toBeCalledWith('groupUuid', 'userUuid')
+		})
+	})
+
+	const groupServiceMock = {
+		findGroupByCode: jest.fn(),
+	}
+	const daoMock = {
+		create: jest.fn(),
+		findByGroupUser: jest.fn(),
+	}
 })
---
+++ test/bills.resolver.e2e-spec.ts
@@ -0,0 +1,131 @@
+import { Test, TestingModule } from '@nestjs/testing'
+import { HttpServer, INestApplication } from '@nestjs/common'
+import { AppModule } from './../src/app.module'
+import { clearDefault, jwtEncoder, sendGqlQuery } from './utils'
+import { USER_TYPE } from '../src/commons/enums/user.enums'
+import { PrismaService } from '../src/commons/prisma/prisma.service'
+import { CURRENCY } from '../src/commons/enums/currency.enums'
+
+describe('BillsResolver (e2e)', () => {
+	let app: INestApplication
+	let server: HttpServer
+	let db: PrismaService
+
+	beforeEach(async () => {
+		await clearDefault()
+		const moduleFixture: TestingModule = await Test.createTestingModule({
+			imports: [AppModule],
+		}).compile()
+		app = moduleFixture.createNestApplication()
+		db = moduleFixture.get<PrismaService>(PrismaService)
+		await app.init()
+		server = app.getHttpServer()
+	})
+
+	afterAll(async () => {
+		await db.$disconnect()
+		await app.close()
+	})
+
+	describe('billCreate', () => {
+		const mutation = `mutation billCreate($input: BillCreateInput!){
+  billCreate(input: $input) {
+    uuid
+    name
+    price
+    payedBy
+  }
+}`
+
+		it('should create bill for logged user', async () => {
+			const user = await db.user.create({
+				data: {
+					name: 'name',
+					email: 'test@mail.com',
+					password: 'Pass',
+					type: USER_TYPE.REGULAR,
+					groups: {
+						create: {
+							partyGroup: {
+								create: {
+									name: 'group',
+									code: 'code',
+									currency: CURRENCY.PLN,
+								},
+							},
+						},
+					},
+				},
+			})
+			const input = {
+				name: 'test',
+				price: 21.37,
+			}
+			const header = jwtEncoder({ ...user, groupCode: 'code' })
+
+			const { body } = await sendGqlQuery(server, mutation, { input }, header)
+
+			expect(body.data.billCreate).toMatchObject({
+				uuid: expect.stringMatching(/.*/),
+				name: input.name,
+				price: input.price,
+				payedBy: user.uuid,
+			})
+		})
+	})
+
+	describe('billGetForGroupUser', () => {
+		const mutation = `mutation billGetForGroupUser{
+  billGetForGroupUser {
+    uuid
+    name
+    price
+    payedBy
+    groupUuid
+  }
+}`
+
+		it('should create bill for logged user', async () => {
+			const user = await db.user.create({
+				data: {
+					name: 'name',
+					email: 'test@mail.com',
+					password: 'Pass',
+					type: USER_TYPE.REGULAR,
+					groups: {
+						create: {
+							partyGroup: {
+								create: {
+									name: 'group',
+									code: 'code',
+									currency: CURRENCY.PLN,
+								},
+							},
+						},
+					},
+				},
+				include: { groups: true },
+			})
+			const bill = await db.bill.create({
+				data: {
+					name: 'test',
+					price: 21.37,
+					payedBy: user.uuid,
+					groupUuid: user.groups[0].groupUuid,
+				},
+			})
+			const header = jwtEncoder({ ...user, groupCode: 'code' })
+
+			const { body } = await sendGqlQuery(server, mutation, undefined, header)
+			expect(body.data.billGetForGroupUser).toMatchObject([
+				{
+					uuid: bill.uuid,
+					name: bill.name,
+					price: bill.price,
+					payedBy: bill.payedBy,
+					groupUuid: bill.groupUuid,
+				},
+			])
+		})
+	})
+})
